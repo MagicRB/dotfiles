a6572e0d4ee5790bf10114e122a9b46d2131ee86
Magic_RB <magic_rb@redalder.org>
2021-05-30 13:19:32 +0200

    Properly support both tweedledee and tweedledum
    Signed-off-by: Magic_RB <magic_rb@redalder.org>

--- nix/hardware/oci-nixos.nix
+++ nix/hardware/oci-nixos.nix
@@ -1,12 +1,16 @@
-hostname:
+{ hostName
+, rootUUID
+, efiUUID
+, swapUUID
+}:
 { config, pkgs, lib, modulesPath, ... }@all:
 with lib;
 let
-  cfg = config.magic_rb.hardware."${hostname}";
+  cfg = config.magic_rb.hardware."${hostName}";
   qemu-guest = import (modulesPath + "/profiles/qemu-guest.nix") all;
 in
 {
-  options.magic_rb.hardware."${hostname}" = mkEnableOption "";
+  options.magic_rb.hardware."${hostName}" = mkEnableOption "";
 
   config = mkIf cfg ({
     boot = {
@@ -24,20 +28,20 @@ in
 
     swapDevices = [
       {
-        device = "/dev/disk/by-uuid/d50e7ebf-8c62-4d2d-b19d-347378f7e5fe";
+        device = "/dev/disk/by-uuid/${swapUUID}";
       } 
     ];
       
     fileSystems = {
       "/" =
         {
-          device = "/dev/disk/by-uuid/79ba4403-7532-4e2c-ac5d-2910dce62009";
+          device = "/dev/disk/by-uuid/${rootUUID}";
           fsType = "xfs";
         };
 
       "/boot/efi" =
         {
-          device = "/dev/disk/by-uuid/4478-6009";
+          device = "/dev/disk/by-uuid/${efiUUID}";
           fsType = "vfat";
         };
     };
