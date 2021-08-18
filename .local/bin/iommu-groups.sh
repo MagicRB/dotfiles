c1ddc6a7c7eaef6f66c664a80013cf8633c5502d
Magic_RB <magic_rb@redalder.org>
2021-08-18 22:14:54 +0200

    Cleanup
    Signed-off-by: Magic_RB <magic_rb@redalder.org>

--- .local/bin/iommu-groups.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-shopt -s nullglob
-for g in /sys/kernel/iommu_groups/*; do
-    echo "IOMMU Group ${g##*/}:"
-    for d in $g/devices/*; do
-        echo -e "\t$(lspci -nns ${d##*/})"
-    done;
-done;
