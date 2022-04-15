da19b8bc35bab89e11cc09587760dd9d8da598b4
main <magic_rb@redalder.org>
2022-04-15 09:51:48 +0200

    Add special ffmpeg with lates SVT-AV1
    Signed-off-by: main <magic_rb@redalder.org>

--- nix/home-manager/modules/cmdline-utils.nix
+++ nix/home-manager/modules/cmdline-utils.nix
@@ -2,6 +2,21 @@
 with lib;
 let
   cfg = config.magic_rb.packageCollections.cmdline;
+
+
+  ffmpegSpecial = pkgs.ffmpeg-full.override
+    { svt-av1 = pkgs.svt-av1.overrideAttrs
+      (old: rec
+      { version = "1.0.0-rc1";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "AOMediaCodec";
+          repo = "SVT-AV1";
+          rev = "v${version}";
+          sha256 = "sha256-iN4ryhoMNlgXWiCup9pNucQKBkmZtC9bj3bsq8l9KYk=";
+        };
+      });
+    };
 in
 {
   options.magic_rb.packageCollections.cmdline = {
@@ -18,6 +33,7 @@ in
       unrar
       git
       htop
+      ffmpegSpecial
     ];
   };
 }
